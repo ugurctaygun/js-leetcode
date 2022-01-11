0771b19ed802c731a7a0b23c0b8f223fbd73eb8f
ugurctaygun <ugurctaygun@gmail.com>
2022-01-11 21:08:25 +0300

    sqrt , climbing-stairs

---
+++ climbing-stairs.js
@@ -0,0 +1,37 @@
+// You are climbing a staircase. It takes n steps to reach the top.
+
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+// Example 1:
+
+// Input: n = 2
+// Output: 2
+// Explanation: There are two ways to climb to the top.
+// 1. 1 step + 1 step
+// 2. 2 steps
+// Example 2:
+
+// Input: n = 3
+// Output: 3
+// Explanation: There are three ways to climb to the top.
+// 1. 1 step + 1 step + 1 step
+// 2. 1 step + 2 steps
+// 3. 2 steps + 1 step
+
+// Constraints:
+
+// 1 <= n <= 45
+
+var climbStairs = function (n) {
+  if (n === 1) return 1;
+  if (n === 2) return 2;
+  let afterOneStep = 2,
+    afterTwoSteps = 1,
+    current;
+  for (let i = 2; i < n; i++) {
+    current = afterOneStep + afterTwoSteps;
+    afterTwoSteps = afterOneStep;
+    afterOneStep = current;
+  }
+  return current;
+};
